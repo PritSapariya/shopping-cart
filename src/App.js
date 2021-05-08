8b36c80082249931e3d297172cb37869dc32ce0e
PritSapariya <pritsapariya17@gmail.com>
2021-05-08 19:08:35 +0530

    Totally Code Changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import GlobalStyle from './globalStyles'
-import { Navbar } from './components'
 
 const App = () => {
   return (
     <>
-      <GlobalStyle />
-      <Navbar />
     </>
   )
 }
